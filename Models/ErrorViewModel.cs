7ce15c76cda8bd6807b2266fae4db5c7bdcbb2e8
eric861129 <74950944+eric861129@users.noreply.github.com>
2021-03-10 18:01:46 +0800

    加入專案檔案。

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LibreriaAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
